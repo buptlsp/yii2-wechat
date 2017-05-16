a7ec6b29ecd55db893111658c09e24d1b8161cd8
lishipeng <lishipeng@ethercap.com>
2017-05-16 16:34:09 +0800

    微信开放平台扫码登录功能

---
+++ src/assets/WxappLoginAsset.php
@@ -0,0 +1,10 @@
+<?php
+namespace lspbupt\wechat\assets;
+
+use yii\web\AssetBundle;
+class WxappLoginAsset extends AssetBundle
+{
+    public $js = [
+        '//res.wx.qq.com/connect/zh_CN/htmledition/js/wxLogin.js',
+    ];
+}
---
+++ src/widgets/JsWxappLogin.php
@@ -0,0 +1,50 @@
+<?php
+namespace lspbupt\wechat\widgets;
+use \Yii;
+use yii\base\Widget;
+use yii\web\JsExpression;
+use yii\base\InvalidConfigException;
+use lspbupt\wechat\WxApp;
+use lspbupt\wechat\assets\WxappLoginAsset;
+class JsWxappLogin extends Widget
+{
+    public $debug = false;
+    public $wxApp = 'wxapp';
+    public $id = 'login_container';
+    public $scope = 'snsapi_login';
+    public $redirect_uri = ''; 
+    public $state = '';
+    public $style = 'black';
+    public $href = '';
+
+    public function init()
+    {
+        if (is_string($this->wxApp)) {
+            $this->wxApp = Yii::$app->get($this->wxApp);
+        } elseif (is_array($this->wxApp)) {
+            if (!isset($this->wxApp['class'])) {
+                $this->wxApp['class'] = WxApp::className();
+            }
+            $this->wxApp = Yii::createObject($this->wxApp);
+        }
+        if (!$this->wxApp instanceof WxApp) {
+            throw new InvalidConfigException("微信配置错误");
+        }
+    }
+
+    public function run()
+    {
+        $view = $this->getView();
+        WxappLoginAsset::register($view);
+        $js = 'var obj = new WxLogin({
+            id:"'.$this->id.'", 
+            appid: "'.$this->wxApp->appid.'", 
+            scope: "'.$this->scope.'", 
+            redirect_uri: "'.$this->redirect_uri.'",
+            state: "'.$this->state.'",
+            style: "'.$this->style.'",
+            href: "'.$this->href.'"
+        }); ';
+        $view->registerJs($js);
+    }
+}
